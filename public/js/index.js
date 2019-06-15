8aeb35204f148fd2d3723baf81a202ea916aa0c0
JoJo <shelton.jocelyn@gmail.com>
2019-06-15 12:12:19 -0400

    update

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,16 @@ var $exampleAuthor = $("#example-author");
 var $submitBtn = $("#submit");
 var $exampleList = $("#example-list");
  
-
+var ingredient = $("#ingr-input").val().trim();
+var appId = "fedaa58c"
+var appKey = "cc278fb637c51da2bed120d07522a08f"
+var queryURL = "https://api.edamam.com/search?q=" + ingredient + "&app_id=fedaa58c" + appId + "&app_key=cc278fb637c51da2bed120d07522a08f" + appKey + "&from=0&to=12";
+$.ajax({
+  url: queryURL,
+  method: 'GET'
+}).then(function(response) {
+  console.log(response)
+});
 
 
 // The API object contains methods for each kind of request we'll make
